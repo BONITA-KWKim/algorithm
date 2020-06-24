043fa4a8e6e0fd2acb1f9821cc03d0d3b10a4983
kyungwon.N.KIM <kyungwon9010@hanmail.net>
2020-06-24 11:47:05 +0900

    Binary tree
    Add functions for binary tree, insert, remove, and searching.
    Binary tree is hard and eciting XD.

--- books/brain-algorithm/search/include/binary-search-tree.h
+++ books/brain-algorithm/search/include/binary-search-tree.h
@@ -1,7 +1,9 @@
 #ifndef __BINARY_SEARCH_TREE_H__
 #define __BINARY_SEARCH_TREE_H__
 
+#include <iostream>
 #include <cstdlib>
+#include <string>
 
 typedef struct tagBSTNode {
     int data;
@@ -21,8 +23,8 @@ public:
         node = (BSTNode *)malloc(sizeof(BSTNode));
 
         node->data = data;
-        node->data = data;
-        node->data = data;
+        node->left = NULL;
+        node->right = NULL;
 
         return node;
     }
@@ -35,14 +37,14 @@ public:
             if (tmp->data > node->data) {
                 if (NULL == tmp->left) {
                     tmp->left = node;
-                    continue;
+                    break;;
                 } else {
-                    tmp = tmp->left
+                    tmp = tmp->left;
                 }
             } else {
                 if (NULL == tmp->right) {
                     tmp->right = node;
-                    continue;
+                    break;
                 } else {
                     tmp = tmp->right;
                 }
@@ -51,26 +53,126 @@ public:
     }
     // delete
     // remove
-    void remove_node (BSTNode *node) {
+    void remove_node (BSTNode *tree, int target) {
+        if(NULL == tree) return;
+
+        BSTNode *parent = NULL;
+        BSTNode *now = tree;
+        while (NULL != now) {
+            if (target > now->data) {
+                // go to right
+                parent = now;
+                now = now->right;
+            } else if (target < now->data) {
+                // go to left
+                parent = now;
+                now = now->left;
+            } else {
+                // working
+                if (NULL == now->left && NULL == now->right) {
+                    // left node
+                    if (parent->left == now) {
+                        parent->left = NULL;
+                    } else {
+                        parent->right = NULL;
+                    }
+                } else {
+                    BSTNode *tmp;
+                    // not left node
+                    if (NULL != now->left && NULL != now->right) {
+                        // not implement
+                        // find smallest node
+                        BSTNode *smallest_node = this->search_min_node(now);
+                        // set the node in now position
+                        if (parent->left == now) {
+                            parent->left = smallest_node;
+                        } else {
+                            parent->right = smallest_node;
+                        }
+                        // remove the node
+                        remove_node(now, smallest_node->data);
+                    } else {
+                        tmp = (NULL != now->left) ? now->left : now->right;
+
+                        if (parent->left == now) {
+                            parent->left = tmp;
+                        } else {
+                            parent->right = tmp;
+                        }
+                    }
+                }
+
+                this->destroy_node(now);
+                break;
+            }
+        }
+
+        return;
+    }
+
+    void destroy_node (BSTNode *node) {
         free(node);
     }
 
-    void remove_tree (BSTNode *node) {
+    void destroy_tree (BSTNode *node) {
         if(NULL != node->right) {
-            remove_node(node->right);
+            this->destroy_node(node->right);
         }
 
         if(NULL != node->left) {
-            remove_node(node->left);
+            this->destroy_node(node->left);
         }
         node->left = NULL;
         node->right = NULL;
         node->data = 0;
-        this->remove_node(node);
+        this->destroy_node(node);
     }
+    // search
+    BSTNode *search_node(BSTNode *root, int target) {
+        if(NULL == root) return NULL;
+
+        BSTNode *result = root;
+        while (NULL != result) {
+            if (target == result->data) {
+                break;
+            } else {
+                if (target > result->data) {
+                    result = result->right;
+                } else {
+                    result = result->left;
+                }
+            }
+        }
+        return result;
+    }
+
+    BSTNode *search_min_node(BSTNode *node) {
+        if(NULL == node) return NULL;
+
+        BSTNode *result = node;
+        while (NULL != result) {
+            result = result->left;
+        }
+
+        return result;
+    }
+
     // print
-    void print_tree (BSTNode *heazd) {
-        return;
+    void inorder_print_tree (BSTNode *node) {
+        if (NULL == node) return;
+
+        inorder_print_tree(node->left);
+        std::cout << node->data << " ";
+        inorder_print_tree(node->right);
+    }
+
+    void inorder_print_tree (std::string *rst, BSTNode *node) {
+        if (NULL == node) return;
+
+        inorder_print_tree(rst, node->left);
+        *rst += std::to_string(node->data);
+        *rst += " ";
+        inorder_print_tree(rst, node->right);
     }
 private:
 };
--- books/brain-algorithm/search/test-search.cc
+++ books/brain-algorithm/search/test-search.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 #include "gtest/gtest.h"
 
 #include "sequential-search.h"
@@ -10,13 +12,43 @@ TEST (SEQUENTIAL, TEST_CASE_001) {
 }
 
 TEST (BINARY_SEARCH_TREE, TEST_CASE_001) {
+    int search_target = 8;
+    std::string tree_elements = "";
+
     BinarySearchTree *bst_tree = new BinarySearchTree();
     BSTNode *head = bst_tree->create_node(1);
-    bst_tree->remove_node(head);
+    BSTNode *node1 = bst_tree->create_node(2);
+    BSTNode *node2 = bst_tree->create_node(3);
+    BSTNode *node3 = bst_tree->create_node(4);
+    BSTNode *node4 = bst_tree->create_node(5);
+    BSTNode *node5 = bst_tree->create_node(6);
+    BSTNode *node6 = bst_tree->create_node(7);
+    BSTNode *node7 = bst_tree->create_node(8);
 
-    EXPECT_EQ(0, head->data);
-    EXPECT_EQ(NULL, head->left);
-    EXPECT_EQ(NULL, head->right);
+    bst_tree->insert_node(head, node1);
+    bst_tree->insert_node(head, node2);
+    bst_tree->insert_node(head, node3);
+    bst_tree->insert_node(head, node4);
+    bst_tree->insert_node(head, node5);
+    bst_tree->insert_node(head, node6);
+    bst_tree->insert_node(head, node7);
+
+    bst_tree->inorder_print_tree (&tree_elements, head);
+    EXPECT_STREQ("1 2 3 4 5 6 7 8 ", tree_elements.c_str());
+    
+    tree_elements = "";
+    bst_tree->remove_node(head, 2);
+    bst_tree->remove_node(head, 4);
+    bst_tree->remove_node(head, 7);
+
+    bst_tree->inorder_print_tree (&tree_elements, head);
+    EXPECT_STREQ("1 3 5 6 8 ", tree_elements.c_str());
+
+    // test search
+    BSTNode *test_search = bst_tree->search_node(head, search_target);
+
+    EXPECT_EQ(search_target, test_search->data);
+
 }
 
 int main (int argc, char *argv[]) {
